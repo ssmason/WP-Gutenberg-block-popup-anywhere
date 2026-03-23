cec6586e0c2c9b914c8ae11caaa94117ba816584
stephen mason <stephenmason@stephens-MBP.OleLan>
2026-03-23 17:41:04 +0100

    chore : change popup overlay color to default white through the block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '52c6b12b34410e052fcc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'a8a07af6947d910e89d0');
